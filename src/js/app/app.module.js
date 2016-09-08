53066f70a029e16421f6213236f355130f20004c
CFE <hello@teamcfe.com>
2016-09-07 17:30:00 -0700

    12 - A Controller & Component

--- src/js/app/app.module.js
+++ src/js/app/app.module.js
@@ -1,3 +1,3 @@
 'use strict';
 
-angular.module('try', []);
+angular.module('try', ['blogList']);
---
+++ src/js/app/blog-list/blog-list.component.js
@@ -0,0 +1,28 @@
+'use strict';
+
+angular.module('blogList').
+    component('blogList', {
+        template: "<div class=''><h1 class='new-class'>{{ title }}</h1><button ng-click='someClickTest()'>Click me!</button></div>",
+        controller: function($scope){
+            $scope.title = 'Hi there'
+            $scope.clicks = 0
+            $scope.someClickTest = function(){
+            console.log("clicked")
+            $scope.clicks += 1
+            $scope.title = 'Clicked ' + $scope.clicks + ' times'
+            }
+        }
+    });
+
+    // controller('BlogListController', function($scope){
+    //     console.log("hello")
+    //     $scope.title = 'Hi there'
+    //     $scope.clicks = 0
+    //     $scope.someClickTest = function(){
+    //         console.log("clicked")
+    //         $scope.clicks += 1
+    //         $scope.title = 'Clicked ' + $scope.clicks + ' times'
+    //     }
+    // });
+
+    // compontent('blogList');
