f20a08481c9a24df518ce640bf9368136b7c3137
CFE <hello@teamcfe.com>
2016-09-10 15:42:07 -0700

    13 - External Templates

--- src/js/app/blog-list/blog-list.component.js
+++ src/js/app/blog-list/blog-list.component.js
@@ -2,14 +2,15 @@
 
 angular.module('blogList').
     component('blogList', {
-        template: "<div class=''><h1 class='new-class'>{{ title }}</h1><button ng-click='someClickTest()'>Click me!</button></div>",
+        // template: "<div class=''><h1 class='new-class'>{{ title }}</h1><button ng-click='someClickTest()'>Click me!</button></div>",
+        templateUrl: '/templates/blog-list.html',
         controller: function($scope){
             $scope.title = 'Hi there'
             $scope.clicks = 0
             $scope.someClickTest = function(){
-            console.log("clicked")
-            $scope.clicks += 1
-            $scope.title = 'Clicked ' + $scope.clicks + ' times'
+                console.log("clicked")
+                $scope.clicks += 1
+                $scope.title = 'Clicked ' + $scope.clicks + ' times'
             }
         }
     });
