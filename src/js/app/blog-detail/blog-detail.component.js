0977ec22dbac8182a4bc05248e1f4ad560ab8ab1
CFE <hello@teamcfe.com>
2016-09-15 14:44:47 -0700

    19 - ForEach Loop in Angular JS Files

--- src/js/app/blog-detail/blog-detail.component.js
+++ src/js/app/blog-detail/blog-detail.component.js
@@ -3,7 +3,7 @@
 angular.module('blogDetail').
     component('blogDetail', {
         templateUrl: '/templates/blog-detail.html',
-        controller: function($routeParams, $scope){
+        controller: function($location, $routeParams, $scope){
             var blogItems = [
                 {title: "Some Title", id: 1, description: "This is a book", publishDate: "2016-09-11"},
                  {title: "Title", id: 2, description: "This is a book"},
@@ -14,5 +14,18 @@ angular.module('blogDetail').
 
             // console.log($routeParams)
             $scope.title = "Blog " + $routeParams.id
+            $scope.notFound = true
+            angular.forEach(blogItems, function(post){
+                if (post.id == $routeParams.id){
+                  $scope.notFound = false
+                  $scope.post = post
+                }
+            })
+
+            if ($scope.notFound) {
+                console.log("Not found")
+                // change location
+                $location.path("/")
+            }
     }
 });
