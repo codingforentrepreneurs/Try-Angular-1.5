3b1b2c999240c8e60f854d3526e97f85e2730154
CFE <hello@teamcfe.com>
2016-09-15 16:00:13 -0700

    21 - ng-resource for mapping to a Restful API

--- src/js/app/blog-detail/blog-detail.component.js
+++ src/js/app/blog-detail/blog-detail.component.js
@@ -3,33 +3,16 @@
 angular.module('blogDetail').
     component('blogDetail', {
         templateUrl: '/templates/blog-detail.html',
-        controller: function($http, $location, $routeParams, $scope){
-            
-
-
-            $http.get("/json/posts.json", {}).then(successCallback, errorCallback);
-
-
-            function successCallback(response, status, config, statusText){
-                $scope.notFound = true
-
-                var blogItems = response.data
-                $scope.posts = blogItems
-
-                angular.forEach(blogItems, function(post){
+        controller: function(Post, $http, $location, $routeParams, $scope){
+            Post.query(function(data){
+              $scope.notFound = true
+               angular.forEach(data, function(post){
                     if (post.id == $routeParams.id){
                       $scope.notFound = false
                       $scope.post = post
                     }
               })
-
-            }
-
-            function errorCallback(response, status, config, statusText){
-                $scope.notFound = true
-                console.log(response)
-            }
-
+            })
 
 
             if ($scope.notFound) {
@@ -39,24 +22,6 @@ angular.module('blogDetail').
             }
 
 
-
-            // var blogItems = [
-            //     {title: "Some Title", id: 1, description: "This is a book", publishDate: "2016-09-11"},
-            //      {title: "Title", id: 2, description: "This is a book"},
-            //       {title: "Tea", id: 3, description: "This is a book"},
-            //        {title: "Lite", id: 4, description: "This is a book"},
-            // ]
-
-
-            // // console.log($routeParams)
-            // $scope.title = "Blog " + $routeParams.id
-            
-            // angular.forEach(blogItems, function(post){
-            //     if (post.id == $routeParams.id){
-            //       $scope.notFound = false
-            //       $scope.post = post
-            //     }
-            // })
 
 
     }
--- src/js/app/blog-list/blog-list.component.js
+++ src/js/app/blog-list/blog-list.component.js
@@ -2,40 +2,8 @@
 
 angular.module('blogList').
     component('blogList', {
-        // template: "<div class=''><h1 class='new-class'>{{ title }}</h1><button ng-click='someClickTest()'>Click me!</button></div>",
         templateUrl: '/templates/blog-list.html',
-        controller: function($routeParams, $scope){
-            console.log($routeParams)
-
-            var blogItems = [
-                {title: "Some Title", id: 1, description: "This is a book", publishDate: "2016-09-11"},
-                 {title: "Title", id: 2, description: "This is a book"},
-                  {title: "Tea", id: 3, description: "This is a book"},
-                   {title: "Lite", id: 4, description: "This is a book"},
-            ]
-
-            $scope.items = blogItems;
-
-
-            $scope.title = 'Hi there'
-            $scope.clicks = 0
-            $scope.someClickTest = function(){
-                console.log("clicked")
-                $scope.clicks += 1
-                $scope.title = 'Clicked ' + $scope.clicks + ' times'
-            }
+        controller: function(Post, $routeParams, $scope){
+            $scope.items = Post.query();
         }
     });
-
-    // controller('BlogListController', function($scope){
-    //     console.log("hello")
-    //     $scope.title = 'Hi there'
-    //     $scope.clicks = 0
-    //     $scope.someClickTest = function(){
-    //         console.log("clicked")
-    //         $scope.clicks += 1
-    //         $scope.title = 'Clicked ' + $scope.clicks + ' times'
-    //     }
-    // });
-
-    // compontent('blogList');
