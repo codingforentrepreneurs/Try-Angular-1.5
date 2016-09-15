216c0e48737f675282ba9da772dd109b2835d620
CFE <hello@teamcfe.com>
2016-09-15 15:21:51 -0700

    20 - HTTP Request in Angular -

--- config.ru
+++ config.ru
@@ -1,5 +1,5 @@
 use Rack::Static,
-      :urls => ["/images", "/js", "/css", "/templates"],
+      :urls => ["/images", "/js", "/css", "/templates", "/json"],
       :root => "src"
 
 run lambda { |env|
--- src/js/app/blog-detail/blog-detail.component.js
+++ src/js/app/blog-detail/blog-detail.component.js
@@ -3,29 +3,61 @@
 angular.module('blogDetail').
     component('blogDetail', {
         templateUrl: '/templates/blog-detail.html',
-        controller: function($location, $routeParams, $scope){
-            var blogItems = [
-                {title: "Some Title", id: 1, description: "This is a book", publishDate: "2016-09-11"},
-                 {title: "Title", id: 2, description: "This is a book"},
-                  {title: "Tea", id: 3, description: "This is a book"},
-                   {title: "Lite", id: 4, description: "This is a book"},
-            ]
+        controller: function($http, $location, $routeParams, $scope){
+            
 
 
-            // console.log($routeParams)
-            $scope.title = "Blog " + $routeParams.id
-            $scope.notFound = true
-            angular.forEach(blogItems, function(post){
-                if (post.id == $routeParams.id){
-                  $scope.notFound = false
-                  $scope.post = post
-                }
-            })
+            $http.get("/json/posts.json", {}).then(successCallback, errorCallback);
+
+
+            function successCallback(response, status, config, statusText){
+                $scope.notFound = true
+
+                var blogItems = response.data
+                $scope.posts = blogItems
+
+                angular.forEach(blogItems, function(post){
+                    if (post.id == $routeParams.id){
+                      $scope.notFound = false
+                      $scope.post = post
+                    }
+              })
+
+            }
+
+            function errorCallback(response, status, config, statusText){
+                $scope.notFound = true
+                console.log(response)
+            }
+
+
 
             if ($scope.notFound) {
                 console.log("Not found")
                 // change location
                 $location.path("/")
             }
+
+
+
+            // var blogItems = [
+            //     {title: "Some Title", id: 1, description: "This is a book", publishDate: "2016-09-11"},
+            //      {title: "Title", id: 2, description: "This is a book"},
+            //       {title: "Tea", id: 3, description: "This is a book"},
+            //        {title: "Lite", id: 4, description: "This is a book"},
+            // ]
+
+
+            // // console.log($routeParams)
+            // $scope.title = "Blog " + $routeParams.id
+            
+            // angular.forEach(blogItems, function(post){
+            //     if (post.id == $routeParams.id){
+            //       $scope.notFound = false
+            //       $scope.post = post
+            //     }
+            // })
+
+
     }
 });
