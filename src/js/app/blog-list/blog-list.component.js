1b0db8e6a2082ebbe729e83396291ec23ad66e64
CFE <hello@teamcfe.com>
2016-09-16 13:49:15 -0700

    23 - ngClick & confirmClick Directives

--- src/js/app/blog-list/blog-list.component.js
+++ src/js/app/blog-list/blog-list.component.js
@@ -3,7 +3,12 @@
 angular.module('blogList').
     component('blogList', {
         templateUrl: '/templates/blog-list.html',
-        controller: function(Post, $routeParams, $scope){
+        controller: function(Post, $location, $routeParams, $rootScope, $scope){
+            $scope.goToItem = function(post){
+                $rootScope.$apply(function(){
+                    $location.path("/blog/" + post.id )
+                })
+            }
             $scope.items = Post.query();
         }
     });
--- src/js/app/utils/confirm-click/confirm-click.directive.js
+++ src/js/app/utils/confirm-click/confirm-click.directive.js
@@ -9,8 +9,12 @@ angular.module("confirmClick").
             var msg = attr.confirmClick || "Are you sure?";
             var clickAction = attr.confirmedClick;
             element.bind('click',function (event) {
+                event.stopImmediatePropagation();
+                event.preventDefault();
                 if ( window.confirm(msg) ) {
                     scope.$eval(clickAction)
+                } else {
+                    console.log("Cancelled")
                 }
             });
         }
