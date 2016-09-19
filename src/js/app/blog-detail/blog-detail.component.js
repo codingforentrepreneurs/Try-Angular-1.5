e8eb1f416ebbfbcca676f60e9aa716ebc0ac3179
CFE <hello@teamcfe.com>
2016-09-19 14:11:51 -0700

    24 - Forms and ngSubmit

--- src/js/app/blog-detail/blog-detail.component.js
+++ src/js/app/blog-detail/blog-detail.component.js
@@ -10,10 +10,25 @@ angular.module('blogDetail').
                     if (post.id == $routeParams.id){
                       $scope.notFound = false
                       $scope.post = post
+                      resetReply()
                     }
               })
             })
 
+
+            $scope.addReply = function() {
+                console.log($scope.reply)
+                $scope.post.comments.push($scope.reply)
+                // $scope.post.comments.push("abc")
+                resetReply()
+            }
+
+            function resetReply(){
+              $scope.reply = {
+                          "id": $scope.post.comments.length + 1,
+                          "text": "",
+              }
+            }
 
             if ($scope.notFound) {
                 console.log("Not found")
