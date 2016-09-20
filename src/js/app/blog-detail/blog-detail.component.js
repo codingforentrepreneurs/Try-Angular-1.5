a25eb1cfe55c7ee061645cde05a35edb1cc476e4
CFE <hello@teamcfe.com>
2016-09-20 15:14:00 -0700

    25 - Confirm Comment Delete

--- src/js/app/blog-detail/blog-detail.component.js
+++ src/js/app/blog-detail/blog-detail.component.js
@@ -15,6 +15,13 @@ angular.module('blogDetail').
               })
             })
 
+            $scope.deleteComment = function(comment) {
+                $scope.$apply(
+                    $scope.post.comments.splice(comment, 1)
+                )
+                // someResource.$delete()
+            }
+
 
             $scope.addReply = function() {
                 console.log($scope.reply)
